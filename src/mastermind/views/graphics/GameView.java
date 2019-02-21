36f4855c32f561333392f1d74454b8dff6116d9e
Tamara <Tamara@192.168.0.17>
2019-02-21 11:19:59 +0100

    Creación del método createProposalPane

--- src/mastermind/views/graphics/GameView.java
+++ src/mastermind/views/graphics/GameView.java
@@ -1,5 +1,6 @@
 package mastermind.views.graphics;
 
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -38,24 +39,29 @@ public class GameView extends JFrame implements ActionListener, KeyListener {
 		for (int i = 0; i < 3; i++) {
 			this.arrayPane[i] = new JPanel();
 			this.arrayPane[i].setLayout(new GridBagLayout());
-		}
-		this.label = new JLabel("Propón una combinación");
-		this.textField = new JTextField(10);
-		this.button = new JButton("Aceptar");
+		}		
 		this.characters = "";
-		this.button.addActionListener(this);
-		this.button.addKeyListener(this);
-		this.getRootPane().setDefaultButton(this.button);
 		this.getContentPane().add(arrayPane[0], new Constraints(0, 0, 3, 1).getConstraints());
 		this.getContentPane().add(arrayPane[1], new Constraints(0, 1, 3, 10).getConstraints());
 		this.getContentPane().add(arrayPane[2], new Constraints(0, 11, 3, 1).getConstraints());
-		this.arrayPane[2].add(label, new Constraints(0, 0, 1, 1).getConstraints());
-		this.arrayPane[2].add(textField, new Constraints(0, 1, 1, 1).getConstraints());
-		this.arrayPane[2].add(button, new Constraints(0, 2, 1, 1).getConstraints());
+		this.createProposalPane();
 		this.setSize(400, 500);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
+	}
+
+	private void createProposalPane() {
+		this.label = new JLabel("Propón una combinación");
+		this.label.setBackground(Color.MAGENTA);
+		this.textField = new JTextField(10);
+		this.button = new JButton("Aceptar");
+		this.button.addActionListener(this);
+		this.button.addKeyListener(this);
+		this.getRootPane().setDefaultButton(this.button);
+		this.arrayPane[2].add(label, new Constraints(0, 0, 1, 1).getConstraints());
+		this.arrayPane[2].add(textField, new Constraints(0, 1, 1, 1).getConstraints());
+		this.arrayPane[2].add(button, new Constraints(0, 2, 1, 1).getConstraints());		
 	}
 
 	public void interact(StartController startController) {
@@ -64,11 +70,6 @@ public class GameView extends JFrame implements ActionListener, KeyListener {
 		this.arrayPane[0].add(new SecretCombinationView(startController.getWidth()));
 		this.arrayPane[1].add(new ProposedCombinationView());
 		this.setVisible(true);
-	}
-
-	private void clear() {
-		this.arrayPane[0].removeAll();
-		this.arrayPane[1].removeAll();
 	}
 
 	public void interact(ProposalController proposalController) {
@@ -80,6 +81,7 @@ public class GameView extends JFrame implements ActionListener, KeyListener {
 			if (error != ProposalController.NO_ERROR && this.characters != "") {
 				new ErrorView(error);
 				error = ProposalController.NO_ERROR;
+				this.characters = "";
 			}
 		} while (error != ProposalController.NO_ERROR || this.characters == "");
 		this.characters = "";
@@ -91,6 +93,11 @@ public class GameView extends JFrame implements ActionListener, KeyListener {
 		arrayPane[1].add(new ResultView(allCodes[allCodes.length - 1][1]),
 				new Constraints(2, turn, 1, 1).getConstraints());
 		this.setVisible(true);
+	}
+
+	private void clear() {
+		this.arrayPane[0].removeAll();
+		this.arrayPane[1].removeAll();
 	}
 
 	@Override
