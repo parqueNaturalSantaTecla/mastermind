8148effe0c8258725d6c9f96d45777eed2b01eaf
Tamara <Tamara@192.168.1.41>
2019-03-13 12:14:53 +0100

    Funcionando con proxy. Añadidos los métodos nuevos en StarControllerProxy heredados de StartController por la adición de GameController.

--- src/mastermind/distributed/StartControllerProxy.java
+++ src/mastermind/distributed/StartControllerProxy.java
@@ -35,4 +35,46 @@ public class StartControllerProxy extends StartController {
 		return names;		
 	}
 
+	@Override
+	public int getTurn() {
+		this.tcpip.send(FrameType.TURN.name());
+		return this.tcpip.receiveInt();
+	}
+
+	@Override
+	public int[][][] getAllCodes() {
+		this.tcpip.send(FrameType.ALLCODES.name());
+		int row = this.tcpip.receiveInt();
+		if (row == 0) {
+			return new int[0][][];
+		}
+		int column = this.tcpip.receiveInt();
+		int array = this.tcpip.receiveInt();
+		int[][][] codes = new int[row][column][];
+		for(int i=0; i<codes.length; i++) {
+			codes[i][0] = new int[array];
+			codes[i][1] = new int[2];
+		}
+		for (int i=0; i<codes.length; i++) {
+			for (int j=0; j<codes[i].length; j++) {
+				for (int k=0; k<codes[i][j].length; k++) {
+					codes[i][j][k] = this.tcpip.receiveInt();
+				}
+			}
+		}
+		return codes;
+	}
+
+	@Override
+	public boolean isWinner() {
+		this.tcpip.send(FrameType.WINNER.name());
+		return this.tcpip.receiveBoolean();
+	}
+
+	@Override
+	public boolean isLooser() {
+		this.tcpip.send(FrameType.LOOSER.name());
+		return this.tcpip.receiveBoolean();
+	}
+
 }
