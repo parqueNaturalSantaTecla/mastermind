210b99adc9c0023bb911765b4ebcc6aa1a9f1037
Tamara <Tamara@192.168.0.11>
2019-06-06 14:02:17 +0200

    R. Reusada clase WithConsoleView desde ErrorView y ColorView

--- src/mastermind/views/MessageView.java
+++ src/mastermind/views/MessageView.java
@@ -3,11 +3,18 @@ package mastermind.views;
 import santaTecla.utils.Console;
 
 enum MessageView {
-	ATTEMPTS("#attempts attempt(s): "), SECRET("*"), RESUME("Do you want to continue"), RESULT(
-			" --> #blacks blacks and #whites whites"), PROPOSED_COMBINATION("Propose a combination: "), TITLE(
-					"----- MASTERMIND -----"), WINNER("You've won!!! ;-)"), LOOSER(
-							"You've lost!!! :-("), PROPOSE_COMMAND("Propose Combination"), UNDO_COMMAND(
-									"Undo previous Proposal"), REDO_COMMAND("Redo previous Proposal"), NEW_LINE("");
+	ATTEMPTS("#attempts attempt(s): "), 
+	SECRET("*"), 
+	RESUME("Do you want to continue"), 
+	RESULT(" --> #blacks blacks and #whites whites"), 
+	PROPOSED_COMBINATION("Propose a combination: "), 
+	TITLE("----- MASTERMIND -----"), 
+	WINNER("You've won!!! ;-)"), 
+	LOOSER("You've lost!!! :-("), 
+	PROPOSE_COMMAND("Propose Combination"), 
+	UNDO_COMMAND("Undo previous Proposal"), 
+	REDO_COMMAND("Redo previous Proposal"), 
+	NEW_LINE("");
 
 	private String message;
 
