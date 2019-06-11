811f5817df0d59be35467bdba53e1cb898ffbcf9
Tamara <Tamara@192.168.0.11>
2019-06-11 17:04:29 +0200

    R. Movido FrameType a mastermind.distributed.dispatchers
    Intentando romper ciclos pero perduran.

--- src/mastermind/distributed/SaveControllerProxy.java
+++ src/mastermind/distributed/SaveControllerProxy.java
@@ -1,6 +1,7 @@
 package mastermind.distributed;
 
 import mastermind.controllers.SaveController;
+import mastermind.distributed.dispatchers.FrameType;
 import mastermind.models.Session;
 import santaTecla.utils.TCPIP;
 
